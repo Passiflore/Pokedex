d2f2c0f52641befa0a39081552eea3453a85e657
Elise <60504990+Passiflore@users.noreply.github.com>
2022-04-25 11:41:25 +0200

    🦁 Fetching all the pokemon from the api

--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -4,17 +4,13 @@ import pokemon from '../../assets/images/bulbizarre.png'
 
 
 function Cards (props) {
-
     return(
-        <div className="cardsBody">
-            {/* <img src={pokemon} className="pokemon" alt="pokemon" />
-            <p className="cardsTitle">Bulbizarre</p>
-            <p className="cardsId">001</p> */}
+        <div className={`cardsBody ${props.pokemonFirstType}`}>
             <img src={props.imgUrl} className="pokemon" alt="pokemon" />
             <p className="cardsTitle">{props.pokemonName}</p>
             <p className="cardsId">{props.pokemonId}</p>
         </div>
     ) 
-};
+}
 
 export default Cards;
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./homePage.css";
 import Cards from "../components/cards/cards";
 import Header from "../components/header/header";
@@ -6,20 +6,67 @@ import DetailsPage from "./detailsPage/detailsPage";
 
 
 
+
 function HomePage () {
+    const [isLoading, setIsLoading] = useState(true);
+    const [pokemonList, setPokemonList] = useState();
+    const [pokemonData, setPokemonData] = useState();
+    let pokemonArray = [];
 
 
-    return(
-        <div className="homeBody">
-            {/* <DetailsPage/> */}
-            <Header/>
-            <div className="cardsContainer">
-                <Cards imgUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg" pokemonName="Ditto" pokemonId="001"/>
-                <Cards imgUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" pokemonName="Bulbasaur" pokemonId="002"/>
 
-            </div>
-        </div>
-    ) 
+
+    // Fetching all the pokemon names from the api
+    const fetchPokemonName = async (next) => {
+        const apiRes = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=1127');
+        const apiJson = await apiRes.json();
+        setPokemonList(apiJson.results)
+    }
+
+    // Fetching all the pokemon data by name
+    const fetchPokemonData = async (pokemonName) => {
+        const apiRes = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+        const apiJson = await apiRes.json()
+        const pokemonObject = {
+            name : pokemonName,
+            sprite : apiJson.sprites.other.dream_world.front_default,
+            id : apiJson.id,
+            fristType : apiJson.types[0].type.name,
+            types : apiJson.types
+        }
+        return pokemonObject;
+    }
+
+    fetchPokemonName();
+    if (pokemonList && isLoading) {
+        pokemonList.forEach(async (element) => {
+            const pokemonFetched = await fetchPokemonData(element.name);
+            pokemonArray.push(pokemonFetched);
+        });
+        setPokemonData(pokemonArray)
+        setIsLoading(false);
+    }
+
+    if (!isLoading) {
+            return(
+                <div className="homeBody">
+                    {/* <DetailsPage/> */}
+                    <Header/>
+                    <div className="cardsContainer">
+                        {
+                            pokemonData.map((pokemon, index) => {
+                                return <Cards imgUrl={pokemon.sprite} pokemonName={pokemon.name} pokemonId={pokemon.id} pokemonFirstType={pokemon.fristType}/>
+                            })
+                        }
+                    
+                        {/* <Cards imgUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg" pokemonName="Ditto" pokemonId="001"/>
+                        <Cards imgUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" pokemonName="Bulbasaur" pokemonId="002"/> */}
+                    </div>
+                </div>
+            ) 
+    }
+
+    
 };
 
 export default HomePage;
